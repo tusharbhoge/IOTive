869e0deb0515dadb85d8ff7d1746b69c64fc3ca1
Tushar <tusharbhoge80@gmail.com>
2025-01-19 23:13:12 +0530

    admin setup is done

---
+++ client/src/components/adminComponents/ViewClient.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ViewClient = () => {
+  return (
+    <div>
+      <h1>View Client Page</h1>
+      <p>Table or list of clients goes here.</p>
+    </div>
+  );
+};
+
+export default ViewClient;
